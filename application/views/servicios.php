e392978769a660d9c615474a848157ba81c4a452
Francisco Javier Sánchez Rosales <aslanlion56@gmail.com>
2013-10-02 16:27:29 -0500

    maquetacion

---
+++ application/views/servicios.php
@@ -0,0 +1,20 @@
+<section class="ac-container">
+    <div>
+        <label>Servicios varios</label>
+    </div>
+    <div>
+        <input id="ac-1" name="accordion-1" type="radio" />
+        <label for="ac-1">About us</label>
+        <article class="ac-small">
+            <p>Some content... </p>
+        </article>
+    </div>
+    <div>
+        <input id="ac-2" name="accordion-1" type="radio" />
+        <label for="ac-2">How we work</label>
+        <article class="ac-medium">
+            <p>Some content... </p>
+        </article>
+    </div>
+    <div><!--...--></div>
+</section>
