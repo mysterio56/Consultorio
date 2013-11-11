2ef564e24c736a1f067984a88afb6c3ba676bd72
Stefania033 <fany8803@hotmail.com>
2013-11-11 16:44:43 -0600

    tipoempleado

--- application/views/sistema/tipo_empleado/agregar.php
+++ application/views/sistema/tipo_empleado/agregar.php
@@ -1,24 +1,33 @@
 <script>
+
 	$(function(){ Valid.tipoEmpleado(); });
 </script>
-<?php
-
-	$attributes = array('id' => 'tipoEmpleadoForm');
-
+	<?php
+        $attributes = array('id' => 'tipoEmpleadoForm');
 	 	echo form_open(null,$attributes);
 
-	 		echo form_label('Codigo del tipo de empleado:');
+	echo '<table class="table_form">';
+	echo'<tr>'; 
+ 	echo'<td  width="100" valing="top">'; 
+	 		echo form_label('Codigo:');
+	 echo'</td>';
+     echo'<td>';
 		 	$data = array(
-		 		'name'  => 'codigo',
-		 		'id'    => 'codigo',
-		 		'value' => set_value('codigo'),
-		 		'style' => 'width:80px'
+             'name'  => 'codigo',
+		 	 'id'    => 'codigo',
+		 	'value' => set_value('codigo'),
+		 	'style' => 'width:80px'
 		 	);
-
-		 	echo form_input($data);
-		 	echo '<br />';
-
-		 	echo form_label('Nombre:');
+		 	 
+             echo form_input($data);
+      echo'</td>';
+      echo '</tr>';
+ 		 
+	echo'<tr>'; 
+ 	echo'<td  width="100" valing="top">'; 
+		 	echo form_label('Nombre:'); 
+		 	echo'</td>';
+            echo'<td>';
 		 	$data = array(
 		 		'name'  => 'nombre',
 		 		'id'    => 'nombre',
@@ -26,9 +35,15 @@
 		 		'value' => set_value('nombre'),
 		 		'style' => 'width:125px'
 		 	);
+   echo form_input($data);
+   echo'</td>';
 
-		 	echo form_input($data);
-
+   echo '</tr>'; 
+ 		
+		 	
+   echo '<br />';
+   echo'<tr>'; 
+   echo'<td colspan= 1>';
 		 	$data = array(
 		 		'name'  => 'agregar',
 		 		'id'    => 'agregar',
@@ -37,6 +52,12 @@
 		 	);
 
 		 	echo form_submit($data);
+		 	echo'</td>';
 
+   echo '</tr>'; 
 	 	echo form_close();
 ?>
+
+  </form>
+</div> 
+</table>
