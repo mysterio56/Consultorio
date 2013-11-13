6d1cf3ab48465c296100afdf4e1e1ed2d33bb085
Stefania033 <fany8803@hotmail.com>
2013-11-13 16:56:40 -0600

    productos

---
+++ application/views/sistema/producto/lista.php
@@ -0,0 +1,115 @@
+<?php $aPermisos = unserialize (PERMISOS); ?>
+<section class="datagrid">
+	<table>
+		<thead>
+			<tr>
+				<th>Código</th>
+				<th>Nombre</th>
+			<?php if(in_array($permisos,$aPermisos['Editar']) ): ?>
+					<th>Editar</th>
+				<?php endif; ?>
+				<th>Activo</th>
+				<th>Eliminar</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+				$nRow = 1;
+				foreach($productos as $producto){
+
+					if (($nRow % 2) == 0) {
+						$rowClass = "even";
+					} else {
+						$rowClass = "odd";
+					}
+
+					echo '<tr class='.$rowClass.'>';
+						echo '<td>'.$producto->codigo.'</td>';
+						echo '<td>'.$producto->nombre.'</td>';
+						if(in_array($permisos,array(2,6,7))){ 
+							echo '<td><a href="'.base_url('product/editar/'.$producto->id).'">
+									  <img src="'.base_url('assets/images/edit.png').'" /></a></td>';
+						}			  
+						$activo = $producto->estatus?'active':'inactive';
+						echo '<td>';
+								if(in_array($permisos,$aPermisos['Editar']) ){
+									echo '<a href="'.base_url('product/status/'.$producto->id).'">
+											<img src="'.base_url('assets/images/'.$activo.'.png').'" />
+										 </a>';
+										}
+                        echo '<td>';
+								if(in_array($permisos,$aPermisos['Eliminar']) ){
+									echo '<a href="'.base_url('product/eliminar/'.$producto->id).'">
+									  <img src="'.base_url('assets/images/delete.png').'"/>
+                                     </a>';
+
+
+								}else{
+									echo '<img src="'.base_url('assets/images/'.$activo.'.png').'" />';
+								}
+
+						echo '</td>';
+					echo '</tr>';
+					$nRow++;
+				}
+			?>
+		</tbody>
+		<?php if($productos->paged->total_pages > 1):?>
+			<tfoot>
+				<tr>
+					<td colspan="100%">
+						<div id="paging">
+							<ul>
+								<?php if($productos->paged->has_previous): ?>
+									<li>
+										<a href="<?= base_url('product/index/1'); ?>">
+											<span>Inicio</span>
+										</a>
+									</li>
+									<li>
+										<a href="<?= base_url('product/index/'.$productos->paged->previous_page); ?>">
+											<span>Anterior</span>
+										</a>
+									</li>
+								<?php endif; ?>
+
+								<?php 
+									for($x = 1; $x <= $productos->paged->total_pages; $x++): 
+										if($paginaActual == $x){
+											$pagActiva = 'active';
+										} else {
+											$pagActiva = '';
+										}
+								?>
+											<li>
+												<a href="<?= base_url('product/index/'.$x); ?>" class="<?= $pagActiva ?>">
+													<span><?= $x; ?></span>
+												</a>
+											</li>
+								<?php endfor; ?>
+
+								<?php if($productos->paged->has_next): ?>
+									<li>
+										<a href="<?= base_url('product/index/'.$productos->paged->next_page); ?>">
+											<span>Siguiente</span>
+										</a>
+									</li>
+									<li>
+										<a href="<?= base_url('product/index/'.$productos->paged->total_pages); ?>">
+											<span>Fin</span>
+										</a>
+									</li>
+								<?php endif; ?>
+							</ul>
+						</div>
+					</tr>
+				</tfoot>
+		<?php endif; ?>
+	</table>
+</section>
+	<?php if(!isset($buscar)): ?>
+	<?php if(in_array($permisos,$aPermisos['Agregar']) ): ?>
+		<a class="abutton" href="<?= base_url('product/agregar') ?>">Agregar</a>
+	<?php endif; ?>
+	<a class="abutton" href="<?= base_url('product/buscar') ?>">Búsqueda Avanzada</a>
+    <?php endif;?>
