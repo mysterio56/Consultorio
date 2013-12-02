5b189ee9ef1069f4dd7063b784d8104e242b73a0
Stefania033 <fany8803@hotmail.com>
2013-12-02 16:50:18 -0600

    2columnas

--- application/views/sistema/pacientes/agregar.php
+++ application/views/sistema/pacientes/agregar.php
@@ -6,18 +6,18 @@
 	$attributes = array('id' => 'pacienteForm');
 
 	 	echo form_open(null,$attributes);
-	 	echo '<table class="table_form">';
+	 	echo '<table class="table_form" CELLSPACING="10">';
 	    echo'<tr>'; 
 	    echo form_label('* Campos Requeridos','campo');
 	    echo'<td width="8%">'; 
 	 		echo form_label('* Código:');
 	 		 echo'</td>';
-	   		 echo'<td width="28%">';
+	   		 echo'<td width="8%">';
 		 	$data = array(
 		 		'name'  => 'codigo',
 		 		'id'    => 'codigo',
 		 		'value' => set_value('codigo'),
-		 		'style' => 'width:30%'
+		 		'style' => 'width:90%'
 		 	);
 
 		 	echo form_input($data);
@@ -26,13 +26,13 @@
  	    echo'<td width="8%">'; 
 		 	echo form_label('*Nombre:');
 		 	echo'</td>';
-            echo'<td colspan="35%">';
+            echo'<td  width="8%">';
 		 	$data = array(
 		 		'name'  => 'nombre',
 		 		'id'    => 'nombre',
 		 		'class' => 'capitalize',
 		 		'value' => set_value('nombre'),
-		 		'style' => 'width:50%'
+		 		'style' => 'width:90%'
 		 	);
 
 		 	echo form_input($data);
@@ -50,7 +50,7 @@
 		 		'id'    => 'apellido_p',
 		 		'class' => 'capitalize',
 		 		'value' => set_value('apellido_p'),
-		 		'style' => 'width:70%'
+		 		'style' => 'width:90%'
 		 	);
 
 		 	echo form_input($data);
@@ -60,13 +60,13 @@
   		 	echo form_label('*Apellido materno:');
 		 	echo'</td>';
 
-		 	echo'<td colspan="35%">';
+		 	echo'<td width="8%">';
 		 	$data = array(
 		 		'name'  => 'apellido_m',
 		 		'id'    => 'apellido_m',
 		 		'class' => 'capitalize',
 		 		'value' => set_value('apellido_m'),
-		 		'style' => 'width:50%'
+		 		'style' => 'width:90%'
 		 	);
 
 		 	echo form_input($data);
@@ -82,13 +82,13 @@
 		 		'name'  => 'email',
 		 		'id'    => 'email',
 		 		'value' => set_value('email'),
-		 		'style' => 'width:70%'
+		 		'style' => 'width:90%'
 		 	);
 
 		 	echo form_input($data);
 		 	echo'</td>';
           
- 	     echo'<td width="8%">';
+ 	     echo'<td width="15%">';
 		 	echo form_label('*Teléfono:');
 		 	echo'</td>';
             echo'<td colspan="35%">';
@@ -96,7 +96,7 @@
 		 		'name'  => 'telefono',
 		 		'id'    => 'telefono',
 		 		'value' => set_value('telefono'),
-		 		'style' => 'width:50%'
+		 		'style' => 'width:80%'
 		 	);
 
 		 	echo form_input($data);
@@ -104,16 +104,16 @@
 		 	echo'</tr>';
            
            echo'</tr>';
-		 	echo'<td width="8%">';
+		 	echo'<td width="25%">';
 		 	
 		    echo form_label('*Celular:');
 		 	echo'</td>';
-            echo'<td colspan="35%">';
+            echo'<td colspan="25%">';
 		 	$data = array(
 		 		'name'  => 'celular',
 		 		'id'    => 'celular',
 		 		'value' => set_value('celular'),
-		 		'style' => 'width:20%'
+		 		'style' => 'width:28%'
 		 	);
 
 		 	echo form_input($data);
@@ -121,7 +121,7 @@
             echo '</tr>';
 
             echo'<tr>';
-		 		echo'<td colspan ="100%" class="standOut">';
+		 		echo'<td  colspan ="100%" class="standOut">';
 		 			echo form_label('Dirección:');
 		 		echo'</td>';
 		 	echo'</tr>';
@@ -129,22 +129,22 @@
 		 	
             ?>
  <tr>
-	<td>
+	<td width="3%">
 	<?= form_label('*Estado:'); ?>
 	</td>
-	<td  colspan="2" width="20%">
-	  	 <select  width="20%" name="estado" id="estado" class="hide" onChange="getMunicipalities();"> </select>
-	  	  <div id="wait_estados" class="wait" width="10%">
+	<td>
+	  	 <select  width="50%" name="estado" id="estado" class="hide" onChange="getMunicipalities();"> </select>
+	  	  <div id="wait_estados" class="wait">
 	  	  	<p>Cargando Estados, por favor espere</p>
 	  	  	</div>
 	</td>
 
-	<td>
+	<td width="3%">
 		 	<?= form_label('*Municipio:'); ?>
 	</td>
 	<td>
-		<select  name="municipio" id="municipio" class="hide" onchange="getPostalCodes();"/></select>
-		 <div id="wait_mun" class="wait hide" width="10%">
+		<select  width="50%" name="municipio" id="municipio" class="hide" onchange="getPostalCodes();"/></select>
+		 <div id="wait_mun" class="wait hide">
 	  	  	<p>Cargando Municipios, por favor espere</p>
 	  	  	</div>
 	</td>
@@ -152,22 +152,22 @@
 
 
 <tr>
-    <td>
+    <td width="25%">
     <?= form_label('*Codigo Postal:');?>
     </td>
-    <td  colspan="2" width="20%">
+    <td  width="25%">
     	<select name="codigo_postal" id="codigo_postal" class="hide" onchange="getColonies();"/></select>
-    	 <div id="wait_cp" class="wait hide" width="10%">
+    	 <div id="wait_cp" class="wait hide" width="25%">
 	  	  	<p>Cargando Codigos Postales, por favor espere</p>
 	  	  	</div>
     </td>
 
-    <td width="10%">
+    <td width="25%">
     	<?= form_label('*Colonia:'); ?>
 	</td>
-	<td>
+	<td width="25%">
 		<select name="colonia" id="colonia" class="hide"/></select>
-		 <div id="wait_col" class="wait hide" width="10%">
+		 <div id="wait_col" class="wait hide" width="25%">
 	  	  	<p>Cargando Colonias, por favor espere</p>
 	  	  	</div>
 	</td>
@@ -175,10 +175,10 @@
 
     <?php
     echo '<tr>';
-	echo '<td>';
+	echo '<td width="25%">';
 		 	echo form_label('*Calle:');
 	echo '</td>';
-	            echo '<td>';
+	            echo '<td width="25%">';
 
 			 	$data = array(
 		 		'name'  => 'calle',
@@ -190,15 +190,15 @@
 		 	echo form_input($data);
 	echo '</td>';
 	
-	echo '<td>';
+	echo '<td width="25%">';
 		 	echo form_label('Número Exterior:');
 	echo '</td>';
-	        echo '<td>';
+	        echo '<td width="25%">';
 		 	$data = array(
 		 		'name'  => 'numero_ext',
 		 		'id'    => 'numero_ext',
 		 		'value' => set_value('numero_ext'),
-		 		'style' => 'width:40%'
+		 		//'style' => 'width:210px'
 		 	);
 	
 		 	echo form_input($data);
@@ -206,15 +206,15 @@
 	echo '</tr>';
 
 echo '<tr>';
-	echo '<td>';
+	echo '<td width="25%">';
 		 	echo form_label('Número interior:');
 	echo '</td>';
-	        echo '<td>';
+	        echo '<td width="25%">';
 		 	$data = array(
 		 		'name'  => 'numero_int',
 		 		'id'    => 'numero_int',
 		 		'value' => set_value('numero_int'),
-		 		'style' => 'width:30%'
+		 		//'style' => 'width:210px'
 		 	);
 		 	
 		 	echo form_input($data);
