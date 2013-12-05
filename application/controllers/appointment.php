6c2c6ca3597806802b8bc1a1edbb936ba61a9fb2
masqweb <bebawend@yahoo.com.mx>
2013-12-05 16:45:14 -0600

    intento

--- application/controllers/appointment.php
+++ application/controllers/appointment.php
@@ -398,8 +398,11 @@ class Appointment extends CI_Controller{
 	public function adicional($id_cita){
 
 		$cita = new Reunion();
+		$servicio = new Servicio();
+
 		$cita->where(array( 'id'     => $id_cita,
 							'estatus'=> 1))->get();
+		
 
 		$data['view']     	  = 'sistema/citas/adicional';
 		$data['return']       = 'appointment';
@@ -416,13 +419,15 @@ class Appointment extends CI_Controller{
 	
 		if($this->input->post()){
 			$ingreso  = new Ingreso();
+			$costos = 0;
+			$servicio->where(array('costo_venta'))->get();
 
 		
 			$ingreso->cita_id	  = $cita->id;
 			$ingreso->producto_id = $this->input->post('producto'); 
 			$ingreso->servicio_id = $this->input->post('servicio');
-			
-
+			$ingreso->costo       = $servicio->costo_venta;
+		
 			$ingreso->estatus	   = 1;			
 			$ingreso->consultorio_id = $this->session->userdata('id_consultorio');
 		    $ingreso->fecha_alta     = date("Y-m-d H:i:s");
