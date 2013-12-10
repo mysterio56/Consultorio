16b19a66e4f7cf8ea4fc67e5df1616c2749a5f11
mysterio56 <aslanlion56@gmail.com>
2013-12-10 12:51:53 -0600

    prueba

--- application/views/menu_header.php
+++ application/views/menu_header.php
@@ -13,18 +13,21 @@
             }
   
             foreach($modulos as $modulo){
+              
+              $modulo->modulo->get();
 
-               $modulo->modulo->get();
-
-               $isParent = '';
-
+              $asub = array();
               foreach($modulo->modulo->all as $submodulo){
-                if(in_array($submodulo->nombre,$aSubmodulo)){
-                    $isParent = 'parent';                  
-                }
+                $asub[$submodulo->id] = $submodulo->nombre;
               }
 
-               echo '<li class="'.$isParent.'"><a href="javascript:void(0)" 
+               
+               $aModulos[$modulo->id]['submodulo'] = $asub;
+               $aModulos[$modulo->id]['nombre'] = $modulo->nombre;
+
+              
+
+              /* echo '<li class="'.$isParent.'"><a href="javascript:void(0)" 
                            onClick = "Tab.newTab(\''.$modulo->nombre.'\',base_url+\''.$modulo->url.'\', \''.$modulo->url.'\' );">'.$modulo->nombre.'
                          </a>';
                          if($isParent == 'parent'){
@@ -36,33 +39,15 @@
                                                <a href="javascript:void(0)"
                                                    onClick = "Tab.newTab(\''.$submodulo->nombre.'\',base_url+\''.$submodulo->url.'\', \''.$submodulo->url.'\' );">'.$submodulo->nombre.'
                                                 </a>
-                                            </li>';
-                                      
-                                  
+                                            </li>'; 
                                 }
                             }
                             echo '</ul>';
                          }
 
-               echo '</li>';
+               echo '</li>';*/
             }
+            print_r($aModulos);
          ?>
-         <!--<li class="parent"><a href="javascript:void(0)">Menu 1</a>
-            <ul>
-               <li><a href="javascript:void(0)" onClick = "Tab.newTab('Submenu 1','http://www.mercadolibre.com.mx/');">Submenu 1</a></li>
-               <li><a href="javascript:void(0)" onClick = "Tab.newTab('Submenu 2','http://www.ibazar.com.mx/');">Submenu 2</a></li>
-               <li><a href="javascript:void(0)" onClick = "Tab.newTab('Submenu 3','http://www.sanborns.com.mx/Paginas/Inicio.aspx');">Submenu 3</a></li>
-               <li><a href="javascript:void(0)" onClick = "Tab.newTab('Submenu 4','http://www.marke.com.mx');">Submenu 4</a></li>
-            </ul>
-         </li>
-         <li><a href="javascript:void(0)" onClick = "Tab.newTab('Menu 2',base_url+'sistema');">Menu 2</a></li>
-         <li><a href="javascript:void(0)" onClick = "Tab.newTab('Menu 3',base_url+'sistema/usuarios');">Menu 3</a></li>
-         <li class="parent"><a href="javascript:void(0)">Menu 4</a>
-            <ul>
-               <li><a href="javascript:void(0)" onClick = "Tab.newTab('Submenu 5','http://masqweb.com');">Submenu 5</a></li>
-               <li><a href="javascript:void(0)" onClick = "Tab.newTab('Submenu 6','http://horeb.org.mx/web/');">Submenu 6</a></li>
-               <li><a href="javascript:void(0)" onClick = "Tab.newTab('Submenu 7','http://www.desarrolloweb.com/');">Submenu 7</a></li>
-            </ul>
-         </li>-->
    </nav>
 </div>
