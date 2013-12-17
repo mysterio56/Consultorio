516562b7d8ab7ea82bec17b464c48791127b7c59
mysterio56 <aslanlion56@gmail.com>
2013-12-17 12:03:00 -0600

    estilos para el login

--- application/views/template.php
+++ application/views/template.php
@@ -1,3 +1,5 @@
 <?php $this->load->view('includes/header'); ?>
 <?php $this->load->view($view); ?>
+<?php if ($this->session->userdata('username')): ?>
 <?php $this->load->view('includes/footer'); ?>
+<?php endif; ?>
